dcdab93392d9a7ce0cef3a71b6018f440c1517c6
andreasfredlund <andreasfredlund91@gmail.com>
2017-02-07 14:02:14 +0100

    fixade inputs

--- test1.c
+++ test1.c
@@ -17,5 +17,8 @@ void main(int argc, char **argv) {
   int graphics = atoi(argv[5]);
   printf("Input:\nN: %d, File: %s, Number of steps: %d \nStepsize: %lf, Graphics on/off: %d ", N, filename, nsteps, delta_t, graphics);
   double* p = (double*)malloc(N*5*sizeof(double));
-  read_doubles_from_file(N*5, p, filename);
+
+  int j= read_doubles_from_file(N*5, p, filename);
+  printf("%d\n", j);
+  printf("%d\n",sizeof(double) );
 }
